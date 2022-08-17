6d4e9239f4cc2b6c8e547fafe6604788ee5c5d04
Manuel Medina <39126102+dotMamu@users.noreply.github.com>
2022-08-17 18:18:50 -0500

    Delete GBSoundDemo directory

--- GBSoundDemo/makeGBDK.bat
+++
@@ -1,3 +0,0 @@
-REM Automatically generated from Makefile
-%ZGB_PATH%\..\env\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o sound.o sound.c
-%ZGB_PATH%\..\env\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o sound.gb sound.o
